8033dd93eb929ab1610183ae470421c025d1fc7e
ArnaezB <borjaarpe@protonmail.com>
2023-01-26 19:49:51 +0000

    game_card and main_act xml finished

---
+++ app/src/main/java/es/arnaezb/gli/ui/MainActivity.kt
@@ -0,0 +1,10 @@
+package es.arnaezb.gli.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
